0a95a343231d3fa7bf767c6481438b01953ce4e7
rahuldate14 <rahuldatedatascience@gmail.com>
2026-04-05 16:46:53 +0530

    Exercise complete

---
+++ Gradient_descent/Exercise.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import math
+
+import os
+os.chdir(r"D:/Tutorial/ML_Tutorial/Gradient_descent")
+
+def predict_using_sklearn(x, y):
+    df = pd.read_csv("test_scores.csv")
+    model = LinearRegression()
+    model.fit(df[['math']], df['cs'])
+    return model.coef_, model.intercept_
+
+def gradient_descent(x, y):
+    m_curr = b_curr = 0
+    iterations = 1000000
+    n = len(x)
+    learning_rate = 0.0002
+    cost_previous = 0
+
+    for i in range(iterations):
+        y_predicted = m_curr * x + b_curr
+        cost = (1/n)*sum((y-y_predicted)**2)
+        md = -(2/n)*sum(x*(y-y_predicted))
+        bd = -(2/n)*sum(y-y_predicted)
+        m_curr = m_curr - learning_rate * md
+        b_curr = b_curr - learning_rate * bd
+        if math.isclose(cost, cost_previous, rel_tol=1e-20):
+            break
+        cost_previous = cost
+
+    return m_curr, b_curr
+
+if __name__ == "__main__":
+    df = pd.read_csv("test_scores.csv")
+    x = np.array(df['math'])
+    y = np.array(df['cs'])
+
+    m,b = gradient_descent(x, y)
+    print(f"Gradient Descent: m = {m:.4f}, b = {b:.4f}")
+
+    m_sklearn, b_sklearn = predict_using_sklearn(x, y)
+    print(f"Sklearn Linear Regression: m = {m_sklearn[0]:.4f}, b = {b_sklearn:.4f}")
